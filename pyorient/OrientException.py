c1875724fe858f8c3d5315f243aa082e81951a61
Niko Usai <niko.usai@it.gucci.com>
2014-02-10 22:16:04 +0100

    completed basic parsing

--- pyorient/OrientException.py
+++ pyorient/OrientException.py
@@ -1,6 +1,13 @@
 # @TODO extend a few more exception to be more descriptive
 class PyOrientException(Exception):
-    pass
+  def __init__(self, message, errors):
+    Exception.__init__(self, message)
+    # errors is an array of tuple made this way:
+    # ( java_exception_class,  message)
+    self.errors = errors
+
+  def __str__(self):
+    return "%s - %s" % (self.message, self.errors[0][1])
 
 class PyOrientConnectionException(PyOrientException):
   pass
