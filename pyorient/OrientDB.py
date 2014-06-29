0c412a75cf3ad31c6ce83615dbe651f5b5bcdc77
gremorian <ostico@gmail.com>
2014-06-29 21:19:29 +0200

    Some majors
    Refactoring on classes ( removed also comments )
    Refactoring on method names and constants
    Test case test_dbuser
    PEP8 coding standard

--- pyorient/OrientDB.py
+++ pyorient/OrientDB.py
@@ -1,42 +1,13 @@
-import re
-import json
+# import re
+# import json
 import os
-import struct
-from OrientTypes import OrientRecordLink, OrientRecord, OrientBinaryObject
-from ORecordCoder import ORecordDecoder, ORecordEncoder
-from OrientException import PyOrientConnectionException, PyOrientDatabaseException
-import socket
+# from OrientTypes import OrientRecordLink, OrientRecord, OrientBinaryObject
+# from ORecordCoder import ORecordDecoder, ORecordEncoder
+from OrientException import *
 
-# Operations
-SHUTDOWN    = chr(1)
-CONNECT     = chr(2)
-DB_OPEN     = chr(3)
-DB_CREATE   = chr(4)
-DB_CLOSE    = chr(5)
-DB_EXIST    = chr(6)
-DB_RELOAD   = chr(73)
-DB_DROP     = chr(7)
-DB_SIZE     = chr(8)
-DB_COUNTRECORDS = chr(9)
-
-
-
-# Types Constants
-BOOLEAN = 1  # Single byte: 1 = true, 0 = false
-BYTE = 2
-SHORT = 3
-INT = 4
-LONG = 5
-BYTES = 6  # Used for binary data.
-STRING = 7
-RECORD = 8
-STRINGS = 9
-
-DB_TYPE_DOCUMENT    = 'document'
-DB_TYPE_GRAPH       = 'graph'
-
-STORAGE_TYPE_LOCAL  = 'local'
-STORAGE_TYPE_MEMORY = 'memory'
+from OrientSocket import OrientSocket
+from OrientPrimitives import *
+from OrientOperations import *
 
 
 def dlog( msg ):
@@ -46,97 +17,6 @@ def dlog( msg ):
     if 'DEBUG' in os.environ:
         if os.environ['DEBUG']:
             print "[DEBUG]:: %s" % msg
-
-
-class OrientSocket(object):
-    """docstring for OrientSocket"""
-
-    def __init__(self, host, port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.s.connect((host, port))
-        except socket.error, e:
-            raise PyOrientConnectionException("Socket Error: %s" % e)
-        self.buffer = ''
-
-    #
-    # Read basic types from socket
-    #
-    def readBool(self):
-        return self.readByte() == 1  # 1 = true, 0 = false
-
-    def readByte(self):
-        return ord(self.s.recv(1))
-
-    def readShort(self):
-        return struct.unpack('!h', self.s.recv(2))[0]
-
-    def readInt(self):
-        return struct.unpack('!i', self.s.recv(4))[0]
-
-    def readLong(self):
-        return struct.unpack('!q', self.s.recv(8))[0]
-
-    def readBytes(self):
-        l = self.readInt()
-        if l == -1:
-            return None
-        return self.s.recv(l)
-
-    def readString(self):
-        return self.readBytes()
-
-    def readRecord(self):
-        # TODO: implent
-        raise Exception("TO implement")
-        pass
-
-    def readStrings(self):
-        n = self.readInt()
-        a = []
-        for i in range(0, n):
-            a.append(self.readString())
-        return a
-
-    #
-    # Write basic types on socket
-    #
-    def putBool(self, b):
-        self.buffer += chr(1) if b else chr(0)
-
-    def putByte(self, c):
-        self.buffer += c
-
-    def putShort(self, num):
-        self.buffer += struct.pack("!h", num)
-
-    def putInt(self, num):
-        self.buffer += struct.pack("!i", num)
-
-    def putLong(self, num):
-        self.buffer += struct.pack("!q", num)
-
-    def putBytes(self, bytes):
-        self.putInt(len(bytes))
-        self.buffer += bytes
-
-    def putString(self, string):
-        self.putBytes(string)
-
-    def putRecord(self):
-        # TODO: implent
-        raise Exception("TO implement")
-
-    def putStrings(self, strings):
-        for s in strings:
-            self.putString(s)
-
-    #
-    # Send and flush the buffer
-    #
-    def send(self):
-        self.s.send(self.buffer)
-        self.buffer = ''
 
 
 #
@@ -195,106 +75,11 @@ class OrientDB(object):
         # version 6: 100% compatible. Before 1.0rc7-SNAPSHOT
         # < version 6: not compatible
 
-        self.session_id = -1
-
-        # If autoconnect is false
+        # If auto-connect is false
         # or we didn't give credential we don't immediately connect
         if autoconnect and user and pwd:
-            self.connect(user, pwd)
-            if self.session_id < 0:
+            if self.connect(user, pwd) < 0:
                 raise PyOrientConnectionException("Not connected to DB")
-
-    #
-    # Prepare a command to be sent on the connection
-    # take a list of fields, it autoguess by the type for string and int
-    # otherwise it expects a tuple with the right type
-    #
-    def make_request(self, operation, fields=[]):
-
-        dlog("Making request: (%d) %s" % (ord(operation), fields))
-
-        # write operation
-        self.conn.putByte(operation)
-
-        # write current session
-        self.conn.putInt(self.session_id)
-
-        # iterate commands
-        for field in fields:
-            if isinstance(field, str):
-                self.conn.putString(field)
-            elif isinstance(field, int):
-                self.conn.putInt(field)
-            else:
-                # tuple with type
-                t, v = field
-                if t == SHORT:
-                    self.conn.putShort(v)
-                elif t == BYTE:
-                    self.conn.putByte(v)
-        # end for
-
-        # send command
-        self.conn.send()
-
-    #
-    # Parse status and eventual errors
-    #
-    def parse_status(self):
-        # get status (0=OK, 1=ERROR)
-        status = not self.conn.readBool()
-
-        # get session id
-        session_id = self.conn.readInt()
-        # todo: check that session is the same??
-
-        errors = []
-
-        if not status:
-            # Parse the error
-            while self.conn.readBool():
-                exception_class = self.conn.readString()
-                exception_message = self.conn.readString()
-                errors.append((exception_class, exception_message))
-
-        return tuple([status, errors])
-
-    #
-    # Parse a response from the server
-    # giving back the raw content of the response
-    #
-    def parse_response(self, types=[]):
-
-        status, errors = self.parse_status()
-        if not status:
-            # null all the expected returns
-            content = [None for t in types]
-            return tuple([status, errors] + content)
-
-        content = []
-
-        for t in types:
-            if t == INT:
-                content.append(self.conn.readInt())
-            elif t == SHORT:
-                content.append(self.conn.readShort())
-            elif t == LONG:
-                content.append(self.conn.readLong())
-            elif t == BOOLEAN:
-                content.append(self.conn.readBool())
-            elif t == BYTE:
-                content.append(self.conn.readByte())
-            elif t == BYTES:
-                content.append(self.conn.readBytes())
-            elif t == STRING:
-                content.append(self.conn.readString())
-            elif t == STRINGS:
-                content.append(self.conn.readStrings())
-            elif t == RECORD:
-                content.append(self.conn.readRecord())
-
-        return tuple([status, errors] + content)
-
 
     # ------------------------ #
     # REQUESTS IMPLEMENTATIONS #
@@ -305,12 +90,12 @@ class OrientDB(object):
     def shutdown(self, user, pwd):
         # todo: make this check a function decorator
         #if not self.is_connected:
-        #    raise PyOrientConnectionException("You must be connected to issue this command", [])
+        # raise PyOrientConnectionException(
+        #   "You must be connected to issue this command", [])
 
-        self.make_request(SHUTDOWN, [user, pwd])
+        self.conn.make_request(SHUTDOWN, [user, pwd])
 
-        # we do not have any respons efor this command :P
-
+        # we do not have any response for this command :P
 
     # CONNECT
     def connect(self, user, pwd):
@@ -319,133 +104,158 @@ class OrientDB(object):
         self.conn = OrientSocket(self.server['host'], int(self.server['port']))
 
         # retrieve protocol version
-        self.protocol_version = self.conn.readShort()
+        self.protocol_version = self.conn.read_short()
         # todo: decide whether give up if protocol is not supported
 
         # packing command
-        self.make_request(CONNECT, ["OrientDB Python client (pyorient)", "1.0", (SHORT, 19), "", user, pwd])
+        self.conn.make_request(CONNECT,
+                               ["OrientDB Python client (pyorient)",
+                                "1.0",
+                                (SHORT, 19),
+                                "",
+                                user,
+                                pwd]
+                               )
 
-        ok, errors, session_id = self.parse_response([INT])
+        ok, errors, session_id = self.conn.parse_response([INT])
         if not ok:
             raise PyOrientConnectionException("Error during connection", errors)
 
         dlog("Session ID: %s" % session_id)
         self.is_connected = True
-        self.session_id = session_id
+        self.conn.set_session_id( session_id )
         return session_id
 
-
     # DB_OPEN
-    def db_open(self, dbname, user, pwd, dbtype=DB_TYPE_DOCUMENT):
+    def db_open(self, db_name, user, pwd, dbtype=DB_TYPE_DOCUMENT):
         # if not init the connection
         if not self.conn:
-            self.conn = OrientSocket(self.server['host'], int(self.server['port']))
-            # retrieve protocol version
-            self.protocol_version = self.conn.readShort()
+            if self.connect(user, pwd) < 0:
+                raise PyOrientConnectionException("Not connected to DB")
 
-        self.make_request(DB_OPEN, [
-            "OrientDB Python client (pyorient)",
-            "1.0",
-            (SHORT, 19),
-            "",
-            dbname,
-            dbtype,
-            user,
-            pwd])
+        self.conn.make_request(DB_OPEN,
+                               ["OrientDB Python client (pyorient)",
+                                "1.0",
+                                (SHORT, 19),
+                                "",
+                                db_name,
+                                dbtype,
+                                user,
+                                pwd])
 
-        # I don't use the helper method parse_response cause dbopen response is fucken strange !
-        status, errors = self.parse_status()
+        # I don't use the helper method parse_response cause
+        # dbopen response is fucken strange !
+        status, errors = self.conn.parse_status()
         if not status:
             # null all the expected returns
-            return tuple([status, errors, None, None, None])
+            raise PyOrientConnectionException( status, errors )
 
-        # Response: (session-id:int)(num-of-clusters:short)
-        # [(cluster-name:string)(cluster-id:short)(cluster-type:string)
-        # (cluster-dataSegmentId:short)] (cluster-config:bytes)(orientdb-release:string)
+        # Response:
+        # (session-id:int)
+        # (num-of-clusters:short)
+        # [
+        #   (cluster-name:string)
+        #   (cluster-id:short)
+        #   (cluster-type:string)
+        #   (cluster-dataSegmentId:short)
+        # ]
+        # (cluster-config:bytes)
+        # (orientdb-release:string)
 
-        # read session
-        self.session_id = self.conn.readInt()
+        # # read session, but we already have one from connect, thrash it
+        _session_id = self.conn.read_int()
+        # print "%r" % _session_id
+        assert _session_id == self.conn.get_session_id()
+
         self.is_connected = True
-        self.opened_db = dbname
-        num_ofcluster = self.conn.readShort()
+        self.opened_db = db_name
+        num_ofcluster = self.conn.read_short()
         clusters = []
 
         for n in range(0, num_ofcluster):
-            cluster_name = self.conn.readString()
-            cluster_id = self.conn.readShort()
-            cluster_type = self.conn.readString()
-            cluster_segmentDataId = self.conn.readShort()
+            cluster_name = self.conn.read_string()
+            cluster_id = self.conn.read_short()
+            cluster_type = self.conn.read_string()
+            cluster_segment_data_id = self.conn.read_short()
             clusters.append({
                 "name": cluster_name,
                 "id": cluster_id,
                 "type": cluster_type,
-                "segment": cluster_segmentDataId
+                "segment": cluster_segment_data_id
             })
 
-        cluster_config = self.conn.readBytes()  #always null
-        self.release = self.conn.readString()
+        cluster_config = self.conn.read_bytes()  # always null
+        self.release = self.conn.read_string()
 
         return clusters
 
     # REQUEST_DB_CREATE
     @need_connected
-    def db_create(self, dbname, dbtype=DB_TYPE_DOCUMENT, storage_type=STORAGE_TYPE_LOCAL):
-        self.make_request(DB_CREATE, [dbname, dbtype, storage_type])
-        ok, errors = self.parse_response()
+    def db_create(self, db_name, dbtype=DB_TYPE_DOCUMENT, storage_type=STORAGE_TYPE_LOCAL):
+        self.conn.make_request(DB_CREATE, [db_name, dbtype, storage_type])
+        ok, errors = self.conn.parse_response()
         return ok
 
     # REQUEST_DB_CLOSE
     @need_db_opened
     def db_close(self):
-        self.make_request(DB_CLOSE)
+        self.conn.make_request(DB_CLOSE)
         return True
 
     # REQUEST_DB_EXIST
     @need_connected
-    def db_exists(self, *args): # dbname, server_storage_type
+    def db_exists(self, *args):  # db_name, server_storage_type
         dbname = args[0]
         server_storage_type = STORAGE_TYPE_LOCAL
         if len(args) > 1:
             server_storage_type = args[1]
 
         if self.protocol_version < 6:
-            self.make_request(DB_EXIST)
+            self.conn.make_request(DB_EXIST)
         elif self.protocol_version < 16:
-            self.make_request(DB_EXIST, [dbname])
+            self.conn.make_request(DB_EXIST, [dbname])
         else:  # > 16 1.5-snapshot
-            self.make_request(DB_EXIST, [dbname, server_storage_type])
+            self.conn.make_request(DB_EXIST, [dbname, server_storage_type])
 
-        ok, errors, exists = self.parse_response([BOOLEAN])
+        ok, errors, exists = self.conn.parse_response([BOOLEAN])
 
         return exists
 
     # REQUEST_DB_RELOAD
     @need_db_opened
     def db_reload(self):
-        self.make_request(DB_RELOAD, [])
-        # Response:(num-of-clusters:short)[(cluster-name:string)(cluster-id:short)(cluster-type:string)(cluster-dataSegmentId:short)]
-        status, errors = self.parse_status()
+        self.conn.make_request(DB_RELOAD, [])
+
+        status, errors = self.conn.parse_status()
         if not status:
             # null all the expected returns
-            return tuple([status, errors, None, None, None])
+            raise PyOrientCommandException( "Reload Failed", errors )
 
-        # Response: (session-id:int)(num-of-clusters:short)
-        # [(cluster-name:string)(cluster-id:short)(cluster-type:string)
-        # (cluster-dataSegmentId:short)] (cluster-config:bytes)(orientdb-release:string)
+        # Response:
+        # (session-id:int)
+        # (num-of-clusters:short)
+        # [
+        #   (cluster-name:string)
+        #   (cluster-id:short)
+        #   (cluster-type:string)
+        #   (cluster-dataSegmentId:short)
+        # ]
+        # (cluster-config:bytes)
+        # (orientdb-release:string)
 
-        num_ofcluster = self.conn.readShort()
+        num_ofcluster = self.conn.read_short()
         clusters = []
 
         for n in range(0, num_ofcluster):
-            cluster_name = self.conn.readString()
-            cluster_id = self.conn.readShort()
-            cluster_type = self.conn.readString()
-            cluster_segmentDataId = self.conn.readShort()
+            cluster_name = self.conn.read_string()
+            cluster_id = self.conn.read_short()
+            cluster_type = self.conn.read_string()
+            cluster_segment_data_id = self.conn.read_short()
             clusters.append({
                 "name": cluster_name,
                 "id": cluster_id,
                 "type": cluster_type,
-                "segment": cluster_segmentDataId
+                "segment": cluster_segment_data_id
             })
         return clusters
 
@@ -458,25 +268,25 @@ class OrientDB(object):
             server_storage_type = args[1]
 
         if self.protocol_version < 16:
-            self.make_request(DB_DROP, [dbname])
+            self.conn.make_request(DB_DROP, [dbname])
         else:  # > 16 1.5-snapshot
-            self.make_request(DB_DROP, [dbname, server_storage_type])
+            self.conn.make_request(DB_DROP, [dbname, server_storage_type])
 
-        ok, errors = self.parse_response([])
+        ok, errors = self.conn.parse_response([])
         return ok
 
     # REQUEST_DB_SIZE
     @need_db_opened
     def db_size(self):
-        self.make_request(DB_SIZE)
-        ok, errors, size = self.parse_response([LONG])
+        self.conn.make_request(DB_SIZE)
+        ok, errors, size = self.conn.parse_response([LONG])
         return size
 
     # REQUEST_DB_COUNTRECORDS
     @need_db_opened
-    def db_countrecords(self):
-        self.make_request(DB_COUNTRECORDS)
-        ok, errors, count = self.parse_response([LONG])
+    def db_count_records(self):
+        self.conn.make_request(DB_COUNTRECORDS)
+        ok, errors, count = self.conn.parse_response([LONG])
         return count
 
     # REQUEST_DATACLUSTER_ADD
@@ -505,7 +315,7 @@ class OrientDB(object):
     # REQUEST_CONFIG_SET
     # REQUEST_CONFIG_LIST
     # REQUEST_DB_RELOAD
-    # REQUEST_DB_LIST
+    # REQUEST_DB_LISTdb_countrecords
     # REQUEST_PUSH_RECORD
     # REQUEST_PUSH_DISTRIB_CONFIG
     # REQUEST_DB_COPY
---
+++ pyorient/OrientSocket.py
@@ -0,0 +1,201 @@
+import struct
+import socket
+from OrientPrimitives import *
+from OrientException import PyOrientConnectionException
+
+
+class OrientSocket(object):
+    """docstring for OrientSocket"""
+
+    def __init__(self, host, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.s.connect((host, port))
+        except socket.error, e:
+            raise PyOrientConnectionException( "Socket Error: %s" % e, e.errno )
+        self.buffer = ''
+        self.__session_id = -1
+
+    def set_session_id(self, session_id):
+        self.__session_id = session_id
+
+    def get_session_id(self):
+        return self.__session_id
+
+    #
+    # Read basic types from socket
+    #
+    def read_bool(self):
+        return self.read_byte() == 1  # 1 = true, 0 = false
+
+    def read_byte(self):
+        return ord(self.s.recv(1))
+
+    def read_short(self):
+        return struct.unpack('!h', self.s.recv(2))[0]
+
+    def read_int(self):
+        return struct.unpack('!i', self.s.recv(4))[0]
+
+    def read_long(self):
+        return struct.unpack('!q', self.s.recv(8))[0]
+
+    def read_bytes(self):
+        l = self.read_int()
+        if l == -1:
+            return None
+        return self.s.recv(l)
+
+    def read_string(self):
+        return self.read_bytes()
+
+    def read_record(self):
+        # TODO: implent
+        raise Exception("TO implement")
+        pass
+
+    def read_strings(self):
+        n = self.read_int()
+        a = []
+        for i in range(0, n):
+            a.append(self.read_string())
+        return a
+
+    #
+    # Write basic types on socketparse_status
+    #
+    def put_bool(self, b):
+        self.buffer += chr(1) if b else chr(0)
+
+    def put_byte(self, c):
+        self.buffer += c
+
+    def put_short(self, num):
+        self.buffer += struct.pack("!h", num)
+
+    def put_int(self, num):
+        self.buffer += struct.pack("!i", num)
+
+    def put_long(self, num):
+        self.buffer += struct.pack("!q", num)
+
+    def put_bytes(self, _bytes):
+        self.put_int(len(_bytes))
+        self.buffer += _bytes
+
+    def put_string(self, string):
+        self.put_bytes(string)
+
+    def put_record(self, record):
+        # TODO: implement
+        raise Exception("TO implement")
+
+    def put_strings(self, strings):
+        for s in strings:
+            self.put_string(s)
+
+    #
+    # Send and flush the buffer
+    #
+    def send(self):
+        self.s.send(self.buffer)
+        self.buffer = ''
+
+    #
+    # Prepare a command to be sent on the connection
+    # take a list of fields, it auto-guess by the type for string and int
+    # otherwise it expects a tuple with the right type
+    #
+    def make_request(self, operation, fields=None):
+
+        if fields is None:
+            fields = []
+
+        # Global function
+        from OrientDB import dlog
+        dlog("Making request: (%d) %s" % (ord(operation), fields))
+
+        # write operation
+        self.put_byte(operation)
+
+        # write current session
+        self.put_int(self.__session_id)
+
+        # iterate commands
+        for field in fields:
+            if isinstance(field, str):
+                self.put_string(field)
+            elif isinstance(field, int):
+                self.put_int(field)
+            else:
+                # tuple with type
+                t, v = field
+                if t == SHORT:
+                    self.put_short(v)
+                elif t == BYTE:
+                    self.put_byte(v)
+        # end for
+
+        # send command[]
+        self.send()
+
+    #
+    # Parse a response from the server
+    # giving back the raw content of the response
+    #
+    def parse_response(self, types=None):
+
+        if types is None:
+            types = []
+
+        status, errors = self.parse_status()
+        if not status:
+            # null all the expected returns
+            content = [None for t in types]
+            return tuple([status, errors] + content)
+
+        content = []
+
+        for t in types:
+            if t == INT:
+                content.append(self.read_int())
+            elif t == SHORT:
+                content.append(self.read_short())
+            elif t == LONG:
+                content.append(self.read_long())
+            elif t == BOOLEAN:
+                content.append(self.read_bool())
+            elif t == BYTE:
+                content.append(self.read_byte())
+            elif t == BYTES:
+                content.append(self.read_bytes())
+            elif t == STRING:
+                content.append(self.read_string())
+            elif t == STRINGS:
+                content.append(self.read_strings())
+            elif t == RECORD:
+                content.append(self.read_record())
+
+        return tuple([status, errors] + content)
+
+    #
+    # Parse status and eventual errors
+    #
+    def parse_status(self):
+        # get status (0=OK, 1=ERROR)
+        status = not self.read_bool()
+
+        # get session id
+        self.__session_id = self.read_int()
+        # todo: check that session is the same??
+
+        errors = []
+
+        if not status:
+            # Parse the error
+            while self.read_bool():
+                exception_class = self.read_string()
+                exception_message = self.read_string()
+                errors.append((exception_class, exception_message))
+
+        return tuple([status, errors])
