fff81b2cf58e4953e2b514b8e6435976d241500d
mogui <mogui83@gmail.com>
2016-06-23 14:19:18 +0200

    - added tox - stepped travis to use python 3.5 - close #160 - close #196 - close #162 with fix as per https://github.com/espeed/bulbs/issues/158#issuecomment-156739706

--- test.py
+++
@@ -1,6 +0,0 @@
-import pyorient
-# db_name = "GratefulDeadConcerts"
-# client = pyorient.OrientDB("localhost", 2424)
-# client.set_session_token(True)
-# cluster_info = client.db_open( db_name, "admin", "admin" )
-# print(client.db_count_records())
