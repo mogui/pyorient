3696ea7936768d2ce68e5217de30983766777112
Marc Auberer <marc.auberer@chillibits.com>
2020-08-06 09:05:08 +0200

    Try to fix tests

--- ci/start-ci.sh
+++ ci/start-ci.sh
@@ -92,7 +92,7 @@ chmod +x ${ODB_LAUNCHER_SYML}
 echo "--- Starting an instance of OrientDB ---"
 if [ -z "${HANG_UP}" ]; then
     #sh -c ${ODB_LAUNCHER_SYML} </dev/null &>/dev/null &
-    sh -c ${ODB_LAUNCHER_SYML}
+    sh -c ${ODB_LAUNCHER_SYML} </dev/null &
     # Wait a bit for OrientDB to finish the initialization phase.
     sleep 5
     printf "\n=== The CI environment has been initialized ===\n"
